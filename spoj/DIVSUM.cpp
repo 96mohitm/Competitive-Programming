7d87957205cefb899082985ed4dc81470bfc2e46
Mohit Musaddi <96mohitm@gmail.com>
2018-07-21 18:36:55 +0530

    Solution to SPOJ DIVSUM - Divisor Summation.

---
+++ spoj/DIVSUM.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef unsigned long long int ul;
+
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define vsort(v) sort(v.begin(), v.end())
+#define get(x) cin >> x
+#define put(x) cout << x
+#define pls(x) cout << x << ' '
+#define pln(x) cout << x << "\n"
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define rep(i, begin, end) \
+  for (__typeof(end) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define watch(x) cout << (#x) << " is " << (x) << endl
+void fast() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+vi d(500001,0);
+//seive like approach.
+void seive(){
+	int i;
+	rep(i,1,500001){
+		for(int j =2*i;j<500001;j=j+i){
+			d[j]+=i;
+		}
+	}
+}
+
+int main(void){
+	fast();
+	seive();
+	int t,n;
+	get(t);
+	while(t--){
+		get(n);
+		pln(d[n]);
+	}
+}
