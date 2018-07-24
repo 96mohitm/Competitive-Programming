5f2c4583de5f5006d800c78d3196ff82809d23cd
Mohit Musaddi <96mohitm@gmail.com>
2018-07-25 04:14:08 +0530

    Solution to codechef THREEDIF Three Different Numbers.

---
+++ codechef/THREEDIF.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef unsigned long long int ul;
+
+#define MOD 1000000007
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define vsort(v) sort(v.begin(), v.end())
+#define get(x) cin >> x
+#define put(x) cout << x
+#define pls(x) cout << x << ' '
+#define pln(x) cout << x << "\n"
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define rep(i, begin, end) \
+  for (__typeof(end) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define watch(x) cout << (#x) << " is " << (x) << endl
+void fast() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+int main(void){
+	fast();
+	int t;
+	get(t);
+	while(t--){
+		ll a[3];
+		get(a[0]);get(a[1]);get(a[2]);
+		sort(a,a+3);
+		if(a[1]<2||a[2]<3){
+			pln("0");
+			continue;
+		}
+		ll r=1;
+		a[1]--;
+		a[2]-=2;
+		r=(((a[0]%MOD)*(a[1]%MOD))%MOD)*(a[2]%MOD);
+		r%=MOD;
+		pln(r);
+	}
+}
