3ebcbdaf812a6dfe8c7e6041647080a756144753
Mohit Musaddi <96mohitm@gmail.com>
2018-08-23 22:57:04 +0530

    Solution to codechef ALTARAY - Alternating subarray prefix.

---
+++ codechef/ALTARAY.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long 		ll;
+typedef long double 	ld;
+typedef vector<int> 	vi;
+typedef vector<ll> 		vl;
+typedef vector<vi> 		vvi;
+typedef vector<ll> 		vl;
+typedef vector<vl> 		vvl;
+typedef pair<int, int> 	pii;
+
+typedef unsigned long long int 	ul;
+
+#define MOD 1000000007
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define vsort(v) sort(v.begin(), v.end())
+#define get(x) cin >> x
+#define put(x) cout << x
+#define pls(x) cout << x << ' '
+#define pln(x) cout << x << "\n"
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define rep(i, begin, end) \
+  for (__typeof(end) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define watch(x) cout << (#x) << " is " << (x) << endl
+void fast() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+int main(void){
+	fast();
+	#ifndef ONLINE_JUDGE
+	 freopen("input.txt", "r", stdin);
+	 freopen("output.txt", "w", stdout);
+	#endif
+	int t;
+	get(t);
+	while(t--){
+		int n,i;
+		get(n);
+		ll a[n],r[n];
+		rep(i,0,n) get(a[i]);
+		r[n-1]=1;
+		rep(i,n-1,0){
+			// pls(i);
+			if(a[i]*a[i+1]<0)
+				r[i] = r[i+1]+1;
+			else
+				r[i]=1;
+		}
+		rep(i,0,n)
+			pls(r[i]);
+		pln("");
+	}
+}
