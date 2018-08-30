7967584b0c3cd3f3932b940833fe1823fab641a1
Mohit Musaddi <96mohitm@gmail.com>
2018-08-30 13:22:37 +0530

    Solution to spoj OLOLO - Onotole needs your help.

---
+++ spoj/OLOLO.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long 		ll;
+typedef long double 	ld;
+typedef vector<int> 	vi;
+typedef vector<ll> 		vl;
+typedef vector<vi> 		vvi;
+typedef vector<ll> 		vl;
+typedef vector<vl> 		vvl;
+typedef pair<int, int> 	pii;
+
+typedef unsigned long long int 	ul;
+
+#define MOD 1000000007
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define vsort(v) sort(v.begin(), v.end())
+#define get(x) cin >> x
+#define put(x) cout << x
+#define pls(x) cout << x << ' '
+#define pln(x) cout << x << "\n"
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define rep(i, begin, end) \
+  for (__typeof(end) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define watch(x) cout << (#x) << " is " << (x) << endl
+void fast() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+int main(void){
+	fast();
+	#ifndef ONLINE_JUDGE
+	 freopen("input.txt", "r", stdin);
+	 freopen("output.txt", "w", stdout);
+	#endif
+	int t,i,x=0;
+	get(t);
+	int a[t];
+	rep(i,0,t){ 
+		get(a[i]);
+		x=x^a[i];
+	}
+	pln(x);
+}
