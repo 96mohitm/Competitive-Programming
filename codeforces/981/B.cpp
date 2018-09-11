4a1c4d652d690f0f40b65ee4fc32268cd75c75ce
Mohit Musaddi <96mohitm@gmail.com>
2018-09-11 23:32:15 +0530

    Codeforces Round #459 (Div. 2)

---
+++ codeforces/981/B.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long 		ll;
+typedef long double 	ld;
+typedef vector<int> 	vi;
+typedef vector<ll> 		vl;
+typedef vector<vi> 		vvi;
+typedef vector<ll> 		vl;
+typedef vector<vl> 		vvl;
+typedef pair<int, int> 	pii;
+
+typedef unsigned long long int 	ul;
+
+#define MOD 1000000007
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define vsort(v) sort(v.begin(), v.end())
+#define get(x) cin >> x
+#define put(x) cout << x
+#define pls(x) cout << x << ' '
+#define pln(x) cout << x << "\n"
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define rep(i, begin, end) \
+  for (__typeof(end) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define watch(x) cout << (#x) << " is " << (x) << endl
+void fast() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+int main(void){
+	fast();
+	#ifndef ONLINE_JUDGE
+	 freopen("input.txt", "r", stdin);
+	 freopen("output.txt", "w", stdout);
+	#endif
+	vector<char> v(1001,'o');
+	v[1]='O';
+	int a,b,c;
+	a=b=1;c=a+b;
+	while(c<1001){
+		v[c]='O';
+		a=b;b=c;
+		c=a+b;
+	}
+	int n,m;
+	get(n);get(m);
+	map<string, string> m;
+	string s1,s2;
+	while(n--){
+		get(s1);get(s2);
+		m[s2]=s1;
+		pln(m[s2]);
+	}
+	// rep(i,1,t+1){
+	// 	put(v[i]);
+	// }
+}
