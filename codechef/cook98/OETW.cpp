a29ac38a156c9fa0f164c749d6c2b68445467248
Mohit Musaddi <96mohitm@gmail.com>
2018-09-29 18:45:02 +0530

    Solution to codechef cook98 - OETW - OneTwo.

---
+++ codechef/cook98/OETW.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long 		ll;
+typedef long double 	ld;
+
+typedef pair<int, int> 	pii;
+typedef vector<int> 	vi;
+typedef vector<pii>		vpi;
+typedef vector<ll> 		vl;
+typedef vector<vi> 		vvi;
+typedef vector<ll> 		vl;
+typedef vector<vl> 		vvl;
+
+typedef unsigned long long int 	ul;
+
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define MOD 1000000007
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define vsort(v) sort(v.begin(), v.end())
+#define get(x) cin >> x
+#define put(x) cout << x
+#define pls(x) cout << x << ' '
+#define pln(x) cout << x << "\n"
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define rep(i, begin, end) \
+  for (__typeof(end) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define watch(x) cout << (#x) << " is " << (x) << endl
+
+void fast() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+
+void solve(){
+	int t;
+	get(t);
+	while(t--){
+		ll n,m,r;
+		get(n);//get(m);
+		int s=0,a[n];
+		rep(i,0,n){
+			get(a[i]);
+			s+=a[i];
+		}
+		if(n*2==s){
+			pln(n);
+			continue;
+		}
+		else{
+			rep(i,0,n){
+				if(a[i]==2&&a[n-i-1]==2)
+					s--;
+				else{
+					break;
+				}
+			}
+		pln(s);
+		}
+	}
+}
+
+int main(void){
+	fast();
+	#ifndef ONLINE_JUDGE
+	 freopen("input.txt", "r", stdin);
+	 freopen("output.txt", "w", stdout);
+	#endif
+	solve();
+}
