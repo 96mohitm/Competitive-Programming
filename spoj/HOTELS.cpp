9c0700beb5ef7381f040160aa76f0e09f0fa38b7
Mohit Musaddi <96mohitm@gmail.com>
2018-11-09 04:19:29 +0530

    spoj HOTELS - Hotels Along the Croatian Coast

---
+++ spoj/HOTELS.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long 		ll;
+typedef long double 	ld;
+
+typedef pair<int, int> 	pii;
+typedef vector<int> 	vi;
+typedef vector<pii>		vpi;
+typedef vector<ll> 		vl;
+typedef vector<vi> 		vvi;
+typedef vector<ll> 		vl;
+typedef vector<vl> 		vvl;
+
+typedef unsigned long long int 	ul;
+
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define MOD 1000000007
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define vsort(v) sort(v.begin(), v.end())
+#define get(x) cin >> x
+#define put(x) cout << x
+#define pls(x) cout << x << ' '
+#define pln(x) cout << x << "\n"
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define rep(i, begin, end) \
+  for (__typeof(end) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define watch(x) cout << (#x) << " is " << (x) << endl
+
+void fast() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+
+void solve(){
+	// int t;
+	// get(t);
+	// while(t--){
+		int n,m;
+		get(n);get(m);
+		// get(m);
+		ll a[n];
+		rep(i,0,n) get(a[i]);
+		ll sum=0;
+		ll r=0;
+		int i=0;
+		deque<ll> dq;
+		while(i<n){
+			if(sum+a[i]<=m){
+				sum+=a[i];
+				if(r<sum)
+					r = sum;
+				dq.push_back(a[i++]);
+			}
+			else if(sz(dq)==0)
+				i++;
+			else{
+				sum-=dq.front();
+				dq.pop_front();
+			}
+			// watch(sum);
+		}
+		pln(r);
+	// }
+}
+
+int main(void){
+	fast();
+	#ifndef ONLINE_JUDGE
+	 freopen("input.txt", "r", stdin);
+	 freopen("output.txt", "w", stdout);
+	#endif
+	solve();
+}
