4ecb7dc4055d7871fb1396c24c176093fdd7c0c0
Mohit Musaddi <96mohitm@gmail.com>
2018-12-29 17:50:31 +0530

    codechef IPCTRAIN

---
+++ codechef/IPCTRAIN.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long 		ll;
+typedef long double 	ld;
+
+typedef pair<int, int> 	pii;
+typedef vector<int> 	vi;
+typedef vector<pii>		vpi;
+typedef vector<ll> 		vl;
+typedef vector<vi> 		vvi;
+typedef vector<ll> 		vl;
+typedef vector<vl> 		vvl;
+
+typedef unsigned long long int 	ul;
+
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define MOD 1000000007
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define vsort(v) sort(v.begin(), v.end())
+#define get(x) cin >> x
+#define put(x) cout << x
+#define pls(x) cout << x << ' '
+#define pln(x) cout << x << "\n"
+#define vget(v) rep(i, 0, v.z) get(v[i]);
+#define rep(i, begin, end) \
+  for (__typeof(end) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define watch(x) cout << (#x) << " is " << (x) << endl
+
+void fast() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+
+
+void solve(){
+	int t;
+	get(t);
+	while(t--){
+		int tr,d;
+		get(tr);get(d);
+		vector<pair<ll, ll> > v[d];
+		for(int i=0;i<tr;i++){
+			ll a,b,c;
+			get(a);get(b);get(c);
+			v[(a -1)].pb({c,b});
+		}
+		priority_queue<pair<ll, ll> > p;
+		for(int i=0;i<d;i++){
+			for(int j=0;j<v[i].size();j++){
+				// watch(v[i][j].fi);
+				// watch(v[i][j].se);
+				p.push(v[i][j]);
+			}
+			// watch(p.top().fi);
+			pair<ll, ll> pq = p.top();
+			if(sz(p)){
+				p.pop();
+				pq.se -=1;
+				if(pq.se)
+					p.push(pq);
+
+			}
+		}
+		ll r =0;
+		while(sz(p)){
+			r += 1LL*p.top().fi * p.top().se;
+			p.pop();
+		}
+		pln(r);
+	}
+}
+
+int main(void){
+	fast();
+	#ifndef ONLINE_JUDGE
+	 freopen("input.txt", "r", stdin);
+	 freopen("output.txt", "w", stdout);
+	#endif
+	solve();
+}
